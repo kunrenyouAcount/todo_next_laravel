99cfbad0adb9dd240b061456a160fc48f2c6d8d2
Takumi-Koyama <t-koyama@ediplex.com>
2021-07-13 16:18:28 +0900

    breezeの実装OK

--- todo/database/migrations/2014_10_12_000000_create_users_table.php
+++ todo/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('email')->unique();
-            $table->string('pass');
             $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
