550f3ac11b6e3d575dee35bc4fcc5731f1c8cb05
kunrenyouAccount <1214kunrenyou@gmail.com>
2021-07-23 17:51:09 +0900

    ログインユーザーからtodoを取得するように処理を修正。READMEを修正。

--- todo/backend/app/Services/Todo/TodoFetchService.php
+++ todo/backend/app/Services/Todo/TodoFetchService.php
@@ -2,7 +2,6 @@
 
 namespace App\Services\Todo;
 
-use \App\Models\User;
 use \App\Models\Todo;
 use Illuminate\Database\Eloquent\Collection;
 
@@ -11,7 +10,7 @@ final class TodoFetchService
     public function main(): Collection
     {
         //1は仮置き
-        $todos = Todo::where('user_id', 1)->get();
+        $todos = Todo::where('user_id', auth()->user()->id)->get();
         return $todos;
     }
 }
